84a018091258c7efa3c17a9c4dfeb55e82069993
Mohamed Imran M <mohamedimran1040@gmail.com>
2024-09-08 18:32:35 +0530

    api views update

--- ecomAPI/ecomapp/api/views.py
+++ ecomAPI/ecomapp/api/views.py
@@ -4,8 +4,8 @@ from .serializers import ProductSerializer,OrderSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset=Product.objects.all()
-    serializer_class = ProductSeriaizer
+    serializer_class = ProductSerializer
     
 class OrderViewSet(viewsets.ModelViewSet):
     queryset=Order.objects.all()
-    serializer_class = OrderSeriaizer
+    serializer_class = OrderSerializer
