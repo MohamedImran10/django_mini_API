44b90f660644bfbbd55cdd212065002f3225a7ef
Mohamed Imran M <mohamedimran1040@gmail.com>
2024-09-08 21:01:08 +0530

    updated

---
+++ chatAPI/chatapp/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from chatapp.models import Message
+from .serializers import MessageSerializer
+
+class MessageViewSet(viewsets.ModelViewSet):
+    
+    
+
+
+
+
